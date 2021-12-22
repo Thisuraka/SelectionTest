a506efbaa74891e2093bce33fef59920c7d1f581
vibudathisuraka <vibudathisuraka@gmail.com>
2021-12-23 04:04:11 +0530

    three

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Roboto'),
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
